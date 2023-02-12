4799549de10c66134a22a4631934a6c5772cd0f0
Sebastian Giraldo <joabgiraldo@gmail.com>
2023-02-12 11:15:47 -0500

    New branch devPrev

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,50 +19,45 @@ const Layout = ({
     <div
       className={`${
         theme
-          ? "h-screen bg-gradient-to-b from-[#e3eaf1] to-white"
-          : "h-screen bg-gradient-to-b from-neutral-900 to-black"
+          ? "h-screen bg-gradient-to-b from-[#ffffff] to-white text-black"
+          : "h-screen bg-gradient-to-b from-[#111010] to-black text-white"
       }`}
     >
-      <Header theme={theme} setTheme={setTheme} lang={lang} setLang={setLang} />
-      <div className="w-full h-full max-h-screen flex justify-center px-3 pb-3 pt-[4.9rem]">
+      {/* <Header theme={theme} setTheme={setTheme} lang={lang} setLang={setLang} /> */}
+      <div className="w-full h-full max-h-screen flex justify-center sm:p-4 lg:py-5 2xl:py-20 py-2 px-3">
         <div className="w-full max-w-4xl 2xl:max-w-6xl flex gap-3 rounded-md relative">
-          <div
-            className={`${
-              theme
-                ? "absolute left-96 top-32 w-60 h-60 rounded-full animate-bg bg-red-500 blur-3xl"
-                : "absolute left-96 top-32 w-60 h-60 rounded-full animate-bg bg-red-500/50 blur-3xl"
-            }`}
-          ></div>
+          <div className="lg:block hidden">
+            <div
+              className={`${
+                theme
+                  ? "absolute left-96 top-32 w-60 h-60 rounded-full animate-bg bg-red-500/50 blur-3xl"
+                  : "absolute left-96 top-32 w-60 h-60 rounded-full animate-bg bg-red-500/30 blur-3xl"
+              }`}
+            ></div>
 
-          <div
-            className={`${
-              theme
-                ? "absolute right-14 top-20 w-20 h-20 rounded-full animate-bg2 bg-blue-300 blur-3xl"
-                : "absolute right-14 top-20 w-20 h-20 rounded-full animate-bg2 bg-blue-300 blur-3xl"
-            }`}
-          ></div>
+            <div
+              className={`${
+                theme
+                  ? "absolute right-14 top-20 w-20 h-20 rounded-full animate-bg2 bg-blue-300 blur-3xl"
+                  : "absolute right-14 top-20 w-20 h-20 rounded-full animate-bg2 bg-blue-300 blur-3xl"
+              }`}
+            ></div>
 
-          <div
-            className={`${
-              theme
-                ? "absolute left-10 bottom-20 w-60 h-60 rounded-full animate-bg3 bg-fuchsia-500/20 blur-xl"
-                : "absolute left-10 bottom-20 w-72 h-60 rounded-full animate-bg3 bg-fuchsia-500/10 blur-xl"
-            }`}
-          ></div>
-          <div
-            className={`${
-              theme
-                ? "absolute left-4 bottom-5 w-5 h-5 rounded-full animate-ping bg-black/20 blur-xl"
-                : "absolute left-4 bottom-5 w-5 h-5 rounded-full animate-ping bg-white/20 blur-xl"
-            }`}
-          ></div>
-          <Sidebar
-            asPath={asPath}
-            theme={theme}
-            setTheme={setTheme}
-            lang={lang}
-            setLang={setLang}
-          />
+            <div
+              className={`${
+                theme
+                  ? "absolute left-10 bottom-20 w-60 h-60 rounded-full animate-bg3 bg-fuchsia-500/20 blur-xl"
+                  : "absolute left-10 bottom-20 w-72 h-60 rounded-full animate-bg3 bg-fuchsia-500/10 blur-xl"
+              }`}
+            ></div>
+            <div
+              className={`${
+                theme
+                  ? "absolute left-4 bottom-5 w-5 h-5 rounded-full animate-ping bg-black/20 blur-xl"
+                  : "absolute left-4 bottom-5 w-5 h-5 rounded-full animate-ping bg-white/20 blur-xl"
+              }`}
+            ></div>
+          </div>
           <motion.div
             initial={{
               y: 100,
@@ -77,10 +72,19 @@ const Layout = ({
             }}
             className={`${
               theme
-                ? "flex flex-col justify-between w-full h-full rounded-md text-black/80 backdrop-blur-3xl"
-                : "flex flex-col justify-between w-full h-full rounded-md text-white/80 backdrop-blur-3xl"
+                ? "flex gap-2 w-full h-full rounded-md text-black/80 backdrop-blur-3xl"
+                : "flex gap-2 w-full h-full rounded-md text-white/80 backdrop-blur-3xl"
             }`}
           >
+            <div className="lg:block hidden">
+              <Sidebar
+                asPath={asPath}
+                theme={theme}
+                setTheme={setTheme}
+                lang={lang}
+                setLang={setLang}
+              />
+            </div>
             {children}
           </motion.div>
         </div>
